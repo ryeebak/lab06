e10ab5b447a5ddad072a5ecd2910b1147314f201
ryeebak <ryeebak@gmail.com>
2023-09-05 12:28:41 +0300

    added something

---
+++ _build/CMakeFiles/solver.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/admin/ryeebak/workspace/projects/lab06/solver_application/equation.cpp" "CMakeFiles/solver.dir/solver_application/equation.cpp.o" "gcc" "CMakeFiles/solver.dir/solver_application/equation.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/admin/ryeebak/workspace/projects/lab06/_build/formatter_ex_lib_dir/CMakeFiles/formatter_ex_lib.dir/DependInfo.cmake"
+  "/home/admin/ryeebak/workspace/projects/lab06/_build/formatter_ex_lib_dir/formatter_lib_dir/CMakeFiles/formatter_lib.dir/DependInfo.cmake"
+  "/home/admin/ryeebak/workspace/projects/lab06/_build/CMakeFiles/solver_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
